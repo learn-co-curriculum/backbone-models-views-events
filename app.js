6a04d36afb51b586fee91899261c08415f19bc3a
Blake Johnson <blake41@gmail.com>
2015-07-09 17:50:25 -0400

    solution

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-var app = {};
+app = {};
 function bootstrap() {
-  // instantiate your view object and append it's HTML to the DOM.
-  // instantiate your model object
-  // pass the model object to the view when you instantiate it
-  attach your view object to the global app object so we have access to it anywhere. 
+  var user = new User();
+  app.formView = new FormView({model : user});
+  app.userData = new UserDataView({model : user});
+  $("body").append(app.formView.el);
 }
---
+++ views/userData.js
@@ -0,0 +1,15 @@
+UserDataView = Backbone.View.extend({
+  initialize : function() {
+    this.listenTo(this.model, "change", this.handleChange);
+  },
+  render : function() {
+    this.$el.append('<h1>User Data Updated!</h1><h2>Users Name is ' + this.model.get("name") + '</h2>');
+    return this;
+  },
+  handleChange : function() {
+    this.render().appendtoDOM();
+  },
+  appendtoDOM : function() {
+    $("body").append(this.el);
+  }
+})
